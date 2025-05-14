01bf43e34fad41014391e6247399fc943595a7f6
Vardandatasciences <vdatasciences@gmail.com>
2025-05-14 12:36:57 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -56,9 +56,9 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME', default='vardaan'),
-        'USER': config('DB_USER', default='vardaan_user'),
-        'PASSWORD': config('DB_PASSWORD', default='vardaan_pass'),
+        'NAME': config('DB_NAME', default='sri'),
+        'USER': config('DB_USER', default='root'),
+        'PASSWORD': config('DB_PASSWORD', default='root'),
         'HOST': config('DB_HOST', default='127.0.0.1'),
         'PORT': config('DB_PORT', default='3306'),
         'TEST': {
