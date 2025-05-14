0871c538f478f7d3a200e76114dbd6461738a6fa
Vardandatasciences <vdatasciences@gmail.com>
2025-05-14 15:48:52 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -1,83 +1,59 @@
 from pathlib import Path
-from decouple import config, Csv
+from decouple import config
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Secret Key & Debug Mode
-SECRET_KEY = config('SECRET_KEY', default='django-insecure-key')
-DEBUG = config('DEBUG', default=True, cast=bool)
+SECRET_KEY = config('SECRET_KEY', default='django-insecure')
 
-# Allowed Hosts
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
+DEBUG = True
+ALLOWED_HOSTS = []
 
-# Installed Apps
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'myapp',
-]
+INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'django.contrib.contenttypes',
+                  'django.contrib.sessions', 'django.contrib.messages', 'django.contrib.staticfiles',
+                  'myapp']
 
-# Middleware
-MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-]
+MIDDLEWARE = ['django.middleware.security.SecurityMiddleware', 'django.contrib.sessions.middleware.SessionMiddleware',
+              'django.middleware.common.CommonMiddleware', 'django.middleware.csrf.CsrfViewMiddleware',
+              'django.contrib.auth.middleware.AuthenticationMiddleware',
+              'django.contrib.messages.middleware.MessageMiddleware',
+              'django.middleware.clickjacking.XFrameOptionsMiddleware']
 
 ROOT_URLCONF = 'myproject.urls'
 
-# Templates
 TEMPLATES = [{
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
     'DIRS': [],
     'APP_DIRS': True,
-    'OPTIONS': {
-        'context_processors': [
-            'django.template.context_processors.debug',
-            'django.template.context_processors.request',
-            'django.contrib.auth.context_processors.auth',
-            'django.contrib.messages.context_processors.messages',
-        ],
-    },
+    'OPTIONS': {'context_processors': [
+        'django.template.context_processors.debug', 'django.template.context_processors.request',
+        'django.contrib.auth.context_processors.auth', 'django.contrib.messages.context_processors.messages',
+    ]},
 }]
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
-# Database - supports both local and CI/CD via env
+from decouple import config
+
+SECRET_KEY = config('SECRET_KEY', default='unsafe-secret-key')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'sri',           # your main DB
-        'USER': 'root',
-        'PASSWORD': 'root',
-        'HOST': '127.0.0.1',     # or 'db' if using Docker service name
-        'PORT': '3306',
-        'TEST': {
-            'NAME': 'test_sri',  # test DB
-        },
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='3306'),
     }
 }
 
-
-# Password Validators (add real validators in prod)
 AUTH_PASSWORD_VALIDATORS = []
 
-# Internationalization
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-# Static files
 STATIC_URL = '/static/'
-
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
