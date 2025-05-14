4f4d655a99965da437d9074184174ee648adfde8
Vardandatasciences <vdatasciences@gmail.com>
2025-05-14 11:15:21 +0530

    Update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -1,60 +1,82 @@
 from pathlib import Path
-from decouple import config
+from decouple import config, Csv
 import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = config('SECRET_KEY', default='django-insecure')
+# Secret Key & Debug Mode
+SECRET_KEY = config('SECRET_KEY', default='django-insecure-key')
+DEBUG = config('DEBUG', default=True, cast=bool)
 
-DEBUG = True
-ALLOWED_HOSTS = []
+# Allowed Hosts
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost', cast=Csv())
 
-INSTALLED_APPS = ['django.contrib.admin', 'django.contrib.auth', 'django.contrib.contenttypes',
-                  'django.contrib.sessions', 'django.contrib.messages', 'django.contrib.staticfiles',
-                  'myapp']
+# Installed Apps
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'myapp',
+]
 
-MIDDLEWARE = ['django.middleware.security.SecurityMiddleware', 'django.contrib.sessions.middleware.SessionMiddleware',
-              'django.middleware.common.CommonMiddleware', 'django.middleware.csrf.CsrfViewMiddleware',
-              'django.contrib.auth.middleware.AuthenticationMiddleware',
-              'django.contrib.messages.middleware.MessageMiddleware',
-              'django.middleware.clickjacking.XFrameOptionsMiddleware']
+# Middleware
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
 
 ROOT_URLCONF = 'myproject.urls'
 
+# Templates
 TEMPLATES = [{
     'BACKEND': 'django.template.backends.django.DjangoTemplates',
     'DIRS': [],
     'APP_DIRS': True,
-    'OPTIONS': {'context_processors': [
-        'django.template.context_processors.debug', 'django.template.context_processors.request',
-        'django.contrib.auth.context_processors.auth', 'django.contrib.messages.context_processors.messages',
-    ]},
+    'OPTIONS': {
+        'context_processors': [
+            'django.template.context_processors.debug',
+            'django.template.context_processors.request',
+            'django.contrib.auth.context_processors.auth',
+            'django.contrib.messages.context_processors.messages',
+        ],
+    },
 }]
 
 WSGI_APPLICATION = 'myproject.wsgi.application'
 
+# Database - supports both local and CI/CD via env
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Pharma',  # Your actual database name
-        'USER': 'root',  # Your DB username
-        'PASSWORD': 'root',  # Your DB password
-        'HOST': '192.168.0.11 ',  # or the appropriate DB host
-        'PORT': '3306',  # Default MySQL port
+        'NAME': config('DB_NAME', default='vardaan'),
+        'USER': config('DB_USER', default='vardaan_user'),
+        'PASSWORD': config('DB_PASSWORD', default='vardaan_pass'),
+        'HOST': config('DB_HOST', default='127.0.0.1'),
+        'PORT': config('DB_PORT', default='3306'),
         'TEST': {
-            'NAME': 'grc',  # The name of the test database
-        },
+            'NAME': 'test_vardaan',
+        }
     }
 }
 
-}
-
+# Password Validators (add real validators in prod)
 AUTH_PASSWORD_VALIDATORS = []
 
+# Internationalization
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
+# Static files
 STATIC_URL = '/static/'
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
